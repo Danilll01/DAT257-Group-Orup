325fbd9f1c685bb11437904da4c7670a6f84be0f
Turbobus <59963570+Turbobus@users.noreply.github.com>
2022-04-05 00:39:03 +0200

    Three uniqe answers is now generated when a new math problem is generated

--- Assets/Scripts/MathGameScripts/MathAnswerGeneratorScript.cs
+++ Assets/Scripts/MathGameScripts/MathAnswerGeneratorScript.cs
@@ -11,6 +11,8 @@ public class MathAnswerGeneratorScript : MonoBehaviour
     [SerializeField] private TextMeshProUGUI answer2txt;
     [SerializeField] private TextMeshProUGUI answer3txt;
 
+    [SerializeField] private int rangeFromCorrectAnswer = 5;
+
     // Start is called before the first frame update
     void Start() {}
 
@@ -19,6 +21,42 @@ public class MathAnswerGeneratorScript : MonoBehaviour
 
     // Generate 3 answers with one correct answer of them
     public void GenerateAnswers(int rightAnswer) {
-        Debug.Log("Fungerar!!");
+        // Generate first random wrong number
+        int firstWrong = generateRandomNumber(rightAnswer);
+        int secondWrong;
+        do {
+            // Generate second wrong number but ensures it is not the same as the first
+            secondWrong = generateRandomNumber(rightAnswer);
+        } while (secondWrong == firstWrong);
+
+
+        switch (Random.Range(0, 3)) {
+            case 0:
+                answer1txt.text = "= " + rightAnswer;
+                answer2txt.text = "= " + firstWrong;
+                answer3txt.text = "= " + secondWrong;
+                break;
+            case 1:
+                answer1txt.text = "= " + firstWrong;
+                answer2txt.text = "= " + rightAnswer;
+                answer3txt.text = "= " + secondWrong;
+                break;
+            default:
+                answer1txt.text = "= " + firstWrong;
+                answer2txt.text = "= " + secondWrong;
+                answer3txt.text = "= " + rightAnswer;
+                break;
+        }
+    }
+
+    private int generateRandomNumber(int rightAnswer) {
+        int bonus;
+        do {
+            // Randomize a number between -5 to 5
+            bonus = Random.Range(0 - rangeFromCorrectAnswer, 1 + rangeFromCorrectAnswer);
+        } while (rightAnswer + bonus < 0 || bonus == 0); // Ensures that the new generated answer is non negative and is not the same as the answer
+
+        // Returns the new wrong answer
+        return rightAnswer + bonus; 
     }
 }
