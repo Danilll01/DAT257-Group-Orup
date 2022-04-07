79051d76e1c1021eb43d82c71cc72e2c65be8a7b
Daniel Persson <perssondaniel01@gmail.com>
2022-04-08 00:32:49 +0200

    Added comments and randomization for different weather

--- Assets/Scripts/WeatherScripts/WeatherController.cs
+++ Assets/Scripts/WeatherScripts/WeatherController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class WeatherController : MonoBehaviour
 {
@@ -15,14 +16,16 @@ public class WeatherController : MonoBehaviour
     public GameObject cloudyObject;
     public GameObject rainyObject;
 
-    
+    // A type for different weather types
     private enum WeatherTypes {Sun, Cloud, Rain};
+
+    // An enum for wind speed. (Not used now)
     private enum WindSpeed {None, Slow, Fast};
-    private SpriteRenderer weatherIconSprite;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Hide all weather objects when starting the game
         HideAllWeather();
     }
 
@@ -34,7 +37,7 @@ public class WeatherController : MonoBehaviour
 
     public void OnTodayClick ()
     {
-        gameMode.text = "Dagens v�der";
+        gameMode.text = "Dagens väder";
 
         SetWeather(WeatherTypes.Sun, WindSpeed.None);
 
@@ -43,19 +46,28 @@ public class WeatherController : MonoBehaviour
 
     public void OnTomorrowClick()
     {
-        gameMode.text = "Morgondagens v�der";
+        gameMode.text = "Morgondagens väder";
         Debug.Log("Tomorrows weather");
     }
 
     public void OnRandomClick()
     {
-        gameMode.text = "Slumpm�ssigt v�der";
+        gameMode.text = "Slumpmässigt väder";
+
+        // Generates a random weather type
+        System.Random random = new System.Random();
+        Array values = Enum.GetValues(typeof(WeatherTypes));
+        WeatherTypes randomWeather = (WeatherTypes)values.GetValue(random.Next(values.Length));
+
+        // Sets weather to the randomly generated weather
+        SetWeather(randomWeather, WindSpeed.None);
         Debug.Log("Random weather");
     }
     
-
+    // Switches to the correct weather object based on inputted weather.
     private void SetWeather(WeatherTypes weather, WindSpeed windSpeed)
     {
+        // Displays the diffenent weather objects in a mutual exclusive way
         switch (weather) 
         {
             case WeatherTypes.Sun:
@@ -68,12 +80,18 @@ public class WeatherController : MonoBehaviour
                 cloudyObject.SetActive(true);
                 Debug.Log("Cloudy");
                 break;
+            case WeatherTypes.Rain:
+                HideAllWeather();
+                rainyObject.SetActive(true);
+                Debug.Log("Rainy");
+                break;
             default:
                 Debug.Log("No work");
                 break;
         }
     }
 
+    // Hides all weather objects
     private void HideAllWeather()
     {
         sunnyObject.SetActive(false);
