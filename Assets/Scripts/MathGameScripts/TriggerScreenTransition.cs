9577e06258989a752a1ac0e4ecd0a1da81a7ec9d
Turbobus <59963570+Turbobus@users.noreply.github.com>
2022-04-23 14:24:31 +0200

    Player path is now set outside screen

--- Assets/Scripts/MathGameScripts/TriggerScreenTransition.cs
+++ Assets/Scripts/MathGameScripts/TriggerScreenTransition.cs
@@ -18,7 +18,7 @@ public class TriggerScreenTransition : MonoBehaviour
     void Update() {}
 
 
-    // Samt sätt location så karaktären springer utanför skärmen.
+    // Fixa så den sätter rätt location för rätt svar när player springer utanför skärmen
     // Glöm ej en transition till allt mellan
     // Se till att man inte kan gå utanför i gången till en annan skärm
 
