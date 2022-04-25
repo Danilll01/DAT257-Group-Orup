adc70d25a3c2655df275384d1485b222a4841614
Turbobus <59963570+Turbobus@users.noreply.github.com>
2022-04-26 00:25:07 +0200

    Pathfinding

---
+++ Assets/Scripts/MainMenuScripts/JumpNodeScript.cs
@@ -0,0 +1,59 @@
+using Pathfinding;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpNodeScript : MonoBehaviour
+{
+
+    [SerializeField] private Transform jumpToNode;
+    [SerializeField] private AnimationCurve jumpCurve = new AnimationCurve();
+    [SerializeField] private float jumpTime = 0.5f;
+
+    private bool isJumping;
+
+    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+
+    public Vector2 JumpFromPosition() {
+        return transform.position;
+    }
+
+    public Vector2 JumpToCoordinates() {
+        return jumpToNode.position;
+    }
+
+    public float GetJumpSpeed() {
+        return Vector2.Distance(transform.position, jumpToNode.position) / jumpTime;
+    }
+
+    public void StartJumpAnimation(Transform agent, Action setNormalAgentSpeed) {
+        if (!isJumping) {
+            isJumping = true;
+            StartCoroutine(Curve(agent, jumpTime, setNormalAgentSpeed));
+        }
+    }
+
+
+
+
+    private IEnumerator Curve(Transform agent, float duration, Action setNormalAgentSpeed) {
+        Vector3 normalValues = agent.localPosition;
+
+        
+        float normalizedTime = 0.0f;
+        while (normalizedTime < 1.0f) {
+            float yOffset = jumpCurve.Evaluate(normalizedTime);
+
+            agent.transform.localPosition = normalValues + (yOffset * Vector3.up);
+            normalizedTime += Time.deltaTime / duration;
+            yield return null;
+        }
+        setNormalAgentSpeed();
+        isJumping = false;
+    }
+}
---
+++ Assets/Scripts/MainMenuScripts/Navigate.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Pathfinding {
+	/// <summary>
+	/// Sets the destination of an AI to the position of a specified object.
+	/// This component should be attached to a GameObject together with a movement script such as AIPath, RichAI or AILerp.
+	/// This component will then make the AI move towards the <see cref="target"/> set on this component.
+	///
+	/// See: <see cref="Pathfinding.IAstarAI.destination"/>
+	///
+	/// [Open online documentation to see images]
+	/// </summary>
+	[UniqueComponent(tag = "ai.destination")]
+	[HelpURL("http://arongranberg.com/astar/docs/class_pathfinding_1_1_a_i_destination_setter.php")]
+	public class Navigate : VersionedMonoBehaviour {
+		/// <summary>The object that the AI should move to</summary>
+		public Transform target;
+		IAstarAI ai;
+
+		[SerializeField] private JumpNodeScript[] jumpNodes;
+
+		void OnEnable() {
+			ai = GetComponent<IAstarAI>();
+			// Update the destination right before searching for a path as well.
+			// This is enough in theory, but this script will also update the destination every
+			// frame as the destination is used for debugging and may be used for other things by other
+			// scripts as well. So it makes sense that it is up to date every frame.
+			if (ai != null) ai.onSearchPath += Update;
+
+		}
+
+		void OnDisable() {
+			if (ai != null) ai.onSearchPath -= Update;
+		}
+
+		/// <summary>Updates the AI's destination every frame</summary>
+		void Update() {
+
+			if (Input.GetMouseButtonDown(0)) {
+				target.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				if (target != null && ai != null) ai.destination = target.position;
+			}
+
+			checkJumpNode();
+
+		}
+
+		private void checkJumpNode() {
+            foreach (JumpNodeScript node in jumpNodes) {
+
+				if (Vector2.Distance(ai.position, node.JumpFromPosition()) < 0.05) {
+					
+					
+					List<Vector3> remPath = new List<Vector3>();
+					ai.GetRemainingPath(remPath, out bool stale);
+					
+					if (Vector2.Distance(remPath[1], node.JumpToCoordinates()) < 0.05) {
+						float normalSpeed = ai.maxSpeed;
+						ai.maxSpeed = node.GetJumpSpeed();
+						node.StartJumpAnimation(transform.GetChild(0),
+							
+							// Lambda to set speed back to normal after jump
+							() => {
+								ai.maxSpeed = normalSpeed;
+							});
+
+					}
+
+                }
+            }
+        }
+
+		// Sets a new path for the agent to pathfind to
+		public void setNewPath(Vector2 newPosVector) {
+			target.position = newPosVector;
+			if (target != null && ai != null) ai.destination = target.position;
+		}
+
+		
+	}
+}
+
