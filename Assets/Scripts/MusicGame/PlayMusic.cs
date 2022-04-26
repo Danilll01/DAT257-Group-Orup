0a050f92632291b470b137bb0f0c95e84c722d93
Daniel Persson <perssondaniel01@gmail.com>
2022-04-26 19:16:24 +0200

    Changed snap point input to parent and initialized arrays

--- Assets/Scripts/MusicGame/PlayMusic.cs
+++ Assets/Scripts/MusicGame/PlayMusic.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 public class PlayMusic : MonoBehaviour
 {
 
-    [SerializeField] private GameObject[] snapPoints;
+    [SerializeField] private GameObject snapPointParent;
     [SerializeField] private int nrNotesPerBeat = 13;
 
-    private int notePos = 1;
-    private int barPos = 1;
+    private GameObject[] snapPoints;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitializeSnapPointsArray();
     }
 
     // Update is called once per frame
@@ -24,11 +23,41 @@ public class PlayMusic : MonoBehaviour
         
     }
 
+    // Initializes snap points array
+    private void InitializeSnapPointsArray()
+    {
+        // Get number of snap points
+        int nrSnapPoints = snapPointParent.transform.childCount;
+        snapPoints = new GameObject[nrSnapPoints];
+
+        // Add all snap points to array
+        for (int i = 0; i < nrSnapPoints; i++)
+        {
+            GameObject child = snapPointParent.transform.GetChild(i).gameObject;
+            snapPoints[i] = child;
+        }
+    }
+
+    public void PlayMusicLoop()
+    {
+        List<GameObject>[] noteSequence = ParseNoteData();
+        foreach (List<GameObject> notes in noteSequence)
+        {
+            Debug.Log(notes.Count);
+        }
+    }
+
 
     private List<GameObject>[] ParseNoteData()
     {
         int nrNotes = snapPoints.Length / nrNotesPerBeat;
-        List<GameObject>[] noteMatrix = new List<GameObject>[nrNotes];
+        List<GameObject>[] noteSequence = new List<GameObject>[nrNotes];
+
+        for (int i = 0; i < noteSequence.Length; i++)
+        {
+            noteSequence[i] = new List<GameObject>();
+        }
+
         foreach (GameObject snapPoint in snapPoints)
         {
             if (snapPoint.transform.childCount < 2)
@@ -39,9 +68,9 @@ public class PlayMusic : MonoBehaviour
             int currBarPos = ParseIntFromObjectName(snapPoint.name, "Bar");
             int currNotePos = ParseIntFromObjectName(snapPoint.name, "Note");
             
-            noteMatrix[4*(currBarPos - 1) + (currNotePos - 1)].Add(snapPoint);
+            noteSequence[4*(currBarPos - 1) + (currNotePos - 1)].Add(snapPoint);
         }
-        return noteMatrix;
+        return noteSequence;
     }
 
     private int ParseIntFromObjectName(string input, string splitWord)
