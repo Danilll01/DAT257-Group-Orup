11205281e73786e368bc780fb7554910e8768672
Daniel <perssondaniel01@gmail.com>
2022-04-26 23:34:35 +0200

    Added comments

--- Assets/Scripts/MusicGame/PlayMusic.cs
+++ Assets/Scripts/MusicGame/PlayMusic.cs
@@ -10,6 +10,7 @@ public class PlayMusic : MonoBehaviour
     [SerializeField] private GameObject marker;
 
     private GameObject[] snapPoints;
+    
 
 
     // Start is called before the first frame update
@@ -48,51 +49,67 @@ public class PlayMusic : MonoBehaviour
         }
     }
 
+    // Waits for a specified amount of time before playing the list of notes (the beat)
     private IEnumerator PlayNoteAfterTime(float time, List<GameObject> notes)
     {
         yield return new WaitForSeconds(time);
 
-        PlayOneNote(notes);
+        PlayNotes(notes);
     }
 
-    private void PlayOneNote(List<GameObject> notes)
+    // Play all notes in a beat
+    private void PlayNotes(List<GameObject> notes)
     {
         foreach (GameObject note in notes)
         {
+            // Skip if the note is null
             if (note == null) continue;
 
+            // Play the sound of the note
             note.GetComponentInChildren<AudioSource>().Play();
         }
     }
 
+    // Parses all note data in to an array of lists where the lists contains all notes that should be played on the same beat.
     private List<GameObject>[] ParseNoteData()
     {
-        int nrNotes = snapPoints.Length / nrNotesPerBeat;
-        List<GameObject>[] noteSequence = new List<GameObject>[nrNotes];
+        // Calculates the number of note beats in the sequence.
+        int nrBeats = snapPoints.Length / nrNotesPerBeat;
+        List<GameObject>[] noteSequence = new List<GameObject>[nrBeats];
 
+        // Initalize a new list for every element in the array
         for (int i = 0; i < noteSequence.Length; i++)
         {
             noteSequence[i] = new List<GameObject>();
         }
 
+        // Go through every snap point 
         foreach (GameObject snapPoint in snapPoints)
         {
+            // If snap point donsn't contain a note, skip it.
             if (snapPoint.transform.childCount < 2)
             {
                 continue;
             }
 
+            // Get the current position of the note
             int currBarPos = ParseIntFromObjectName(snapPoint.name, "Bar");
             int currNotePos = ParseIntFromObjectName(snapPoint.name, "Note");
             
+            // Add the snap point to the current beat in the note sequence
             noteSequence[4*(currBarPos - 1) + (currNotePos - 1)].Add(snapPoint);
         }
         return noteSequence;
     }
 
+    // Parses int from specialized note name
+    // Ex f("Bar1-Note2-C3", "Bar") parses to 1
     private int ParseIntFromObjectName(string input, string splitWord)
     {
+        // Choose to parse bar or note value
         int splitWordIndex = splitWord == "Bar" ? 0 : 1;
+
+        // Parses the int from string
         return int.Parse(input.Split("-")[splitWordIndex].Split(splitWord)[1]);
     }
 }
