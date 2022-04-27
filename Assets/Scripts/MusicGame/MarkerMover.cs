236249856be997b7c469e894cd925858d682d006
Daniel Persson <perssondaniel01@gmail.com>
2022-04-27 14:46:43 +0200

    Added method for starting marker movement

--- Assets/Scripts/MusicGame/MarkerMover.cs
+++ Assets/Scripts/MusicGame/MarkerMover.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class MarkerMover : MonoBehaviour
 {
 
-    [SerializeField] private GameObject checkpoints;
+    [SerializeField] private GameObject[] jumpNodes;
+    [SerializeField] private GameObject playerWrapper;
+
+    [SerializeField] private Transform originalPos;
+    [SerializeField] private Transform endPos;
+
+    private Navigate playerNavigateScript;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerNavigateScript = playerWrapper.GetComponent<Navigate>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void StartMarkerMovement()
+    {
+        playerNavigateScript.setNewPath((Vector2) endPos.position);
+        //playerNavigateScript.setNewPath((Vector2) originalPos.position);
         
     }
 }
