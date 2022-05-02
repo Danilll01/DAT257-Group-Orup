f330063f00629d661af309cba33281e253f6fa37
elinhagman <73999747+elinhagman@users.noreply.github.com>
2022-05-02 11:23:23 +0200

    PortalCollision updates lastPortalSceneIndex in StoredPortal class

--- Assets/Scripts/MainMenuScripts/PortalCollision.cs
+++ Assets/Scripts/MainMenuScripts/PortalCollision.cs
@@ -5,20 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class PortalCollision : MonoBehaviour
 {
-
+    // this portal's game's scene index
     [SerializeField] private int sceneIndex;
 
     // If collider is triggered, open its corresponding game's scene 
     void OnTriggerEnter2D(Collider2D col)
     {
+        // If player collides with this game object
         if (col.tag == "Player")
         {
+            // Save portal index to lastPortal
+            StorePortal.setLastPortalSceneIndex(sceneIndex);
+            // Deactivate collider
             gameObject.transform.GetComponent<BoxCollider2D>().isTrigger = false;
             // Change to game's scene
             SceneManager.LoadScene(sceneIndex);
 
         }
-        
 
     }
 
