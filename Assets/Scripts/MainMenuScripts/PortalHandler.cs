058514ac33db788626570225a42c7bdcc1f58d57
Turbobus <59963570+Turbobus@users.noreply.github.com>
2022-05-18 16:37:58 +0200

    Added portal sound when clicked and when entering

--- Assets/Scripts/MainMenuScripts/PortalHandler.cs
+++ Assets/Scripts/MainMenuScripts/PortalHandler.cs
@@ -11,21 +11,28 @@ public class PortalHandler : MonoBehaviour
 
     // List of nodes close to each portal
     [SerializeField] private Transform[] portals;
+    [SerializeField] private AudioClip[] portalSound;
     // The player object
     [SerializeField] private Navigate navigation;
+
+
+    private AudioSource audioSource;
 
 
 
 
     public void onPortalClick(int portalId)
     {
+        audioSource.Stop();
+        audioSource.PlayOneShot(portalSound[portalId - 1]);
+
         // Make player go to the portal's corresponding node
         //navigation.setNewPath(portals[portalId].position);
-
         navigation.MoveTowardsPortal(portals[portalId-1].position,
             
             () => {
 
+                audioSource.Play();
                 StorePortal.setLastPortalSceneIndex(portalId);
                 SceneManager.LoadScene(portalId);
 
@@ -37,13 +44,15 @@ public class PortalHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        audioSource = GetComponent<AudioSource>();
+
         // if player(navigation) is coming from a portal, set its position to this portals node
         if (!(StorePortal.getLastPortalSceneIndex() == 0))
         {
             Transform startPos = portals[getPortalsIndex(StorePortal.getLastPortalSceneIndex())];
             navigation.transform.position = startPos.position;
         }
-        
     }
 
     // Translates scene index of the game to its index in portals 
